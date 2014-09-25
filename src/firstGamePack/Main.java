57fb46efbf42a58f91d5884410f99c01d04bf480
Tango <55555.gold@gmail.com>
2014-09-25 17:47:12 +0300

    first commit

--- src/firstGamePack/Main.java
+++ src/firstGamePack/Main.java
@@ -1,10 +1,18 @@
 package firstGamePack;
 
+import javax.swing.*;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		JFrame f = new JFrame("My first Game");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setBounds(0, 0, 1100, 600);   //f.setSize(600,600);
+		f.add(new Road());
+		f.setVisible(true);  
+	}	
 }
